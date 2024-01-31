6dd2d925d0ba3d337567efcc03ba3a8901426f4e
davidsanders <dsanders_m633@outlook.com>
2024-01-30 22:17:15 -0500

    updated pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,64 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    minify: true,
+    outDir: "./dist/",
+    sourcemap: true,
+    emptyOutDir: false,
+    lib: {
+      entry: "./package/index.ts",
+      name: "NeuronDevtools",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "@sandstack/neuron",
+        "react-json-view",
+      ],
+      output: [
+        {
+          dir: "./dist",
+          name: "index",
+          entryFileNames: "index.js",
+        },
+        {
+          dir: "./dist",
+          name: "index",
+          format: "cjs",
+          entryFileNames: "index.cjs",
+        },
+        {
+          dir: "./dist/umd/",
+          name: "index",
+          format: "umd",
+          entryFileNames: "index.js",
+        },
+        {
+          dir: "./dist/esm/",
+          name: "index",
+          format: "esm",
+          entryFileNames: "index.js",
+        },
+        {
+          dir: "./dist/iife/",
+          name: "index",
+          format: "iife",
+          entryFileNames: "index.js",
+        },
+        {
+          dir: "./dist/system/",
+          name: "index",
+          format: "system",
+          entryFileNames: "index.js",
+        },
+      ],
+    },
+  },
+  plugins: [react(), dts()],
 });
